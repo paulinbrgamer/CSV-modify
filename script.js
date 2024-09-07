b553614f5e0ecb775ff2b1bdbafa7895d75bbc8e
oPaulin <pauloferadosgames@gmail.com>
2024-09-07 19:14:01 -0300

    comit

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 const input = document.querySelector('#dados')
-var dados;
-var linhas = []
-var inicio= 0
-var string = '';
-var retirada = 0
 input.addEventListener('change',function(){
     const arquivos = this.files[0]
     const reader = new FileReader()
     reader.addEventListener('load',function(){
+        var dados;
         dados = reader.result
-       for (var v = 0; v < dados.length;v++){
-            if (dados[v] == ',' ){
-                for (inicio; inicio<v ;inicio++){
-                    if (dados[inicio] === '\n' || dados[inicio] === '\r'){
-                    v = inicio-1
-                    retirada += 2
-                    }
-                    else{
-                        string = string+dados[inicio]
-                        console.log(dados[inicio])
-                    }
-                    
-                        
-                    
-                
-                }
-                linhas.push(string)
-                string = ''
-                inicio++
-                
-            }
-        if (v == dados.length-retirada-1){
-            linhas.push(dados[dados.length-retirada-1])
-        }
-        
-        
-
-        
-       }
-       console.log(linhas)
+        array = Csv_reader(dados)
+        console.log(array)
     })
     reader.readAsText(arquivos)
 })
+
+function Csv_reader(data){
+    var linhas = []
+    var inicio= 0
+    var string = '';
+    var retirada = 0
+    for (var v = 0; v < data.length;v++){
+        if (data[v] == ',' ){
+            for (inicio; inicio<v ;inicio++){
+                string = string+data[inicio]
+                console.log(dados[inicio])
+
+            }
+            linhas.push(string)
+            string = ''
+            inicio++
+            
+        }
+        if (v == data.length-retirada-1){
+            linhas.push(data[dados.length-retirada-1])
+        }
+   }
+   
+    return linhas
+}
